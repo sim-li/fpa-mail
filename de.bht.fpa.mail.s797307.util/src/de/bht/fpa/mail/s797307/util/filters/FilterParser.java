7c4d65c6eec5e436118ce24817f5b901c5d5c1ba
Simon Lischka <simon@a-studios.org>
2014-01-13 00:10:14 +0100

    Working isChecks

--- de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/filters/FilterParser.java
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/filters/FilterParser.java
@@ -1,93 +1,50 @@
 package de.bht.fpa.mail.s797307.util.filters;
 
 import java.util.LinkedList;
-// public ImportanceFilter(IFilter filter, Importance importance)
-// public IntersectionFilter(IFilter... filters)
-// public ReadFilter(IFilter filter, Boolean isRead)
-// public RecipientsFilter(IFilter filter, String searchString,
-// FilterOperator operator)
-// public SenderFilter(IFilter filter, String searchString, FilterOperator
-// operator)
-// public SubjectFilter(IFilter filter, String searchString, FilterOperator
-// operator)
-// public TextFilter(IFilter filter, String searchString, FilterOperator
-// operator)
-// public UnionFilter(IFilter... filters)
-// String in =
-// "Union(Sender(\"me@this.com\",contains),Recipient(\"foo@bar.de\",is))";
+
+import de.bht.fpa.mail.s000000.common.filter.IFilter;
+import de.bht.fpa.mail.s797307.util.FilterOperator;
+import de.bht.fpa.mail.s797307.util.FilterType;
 
 public final class FilterParser {
-  /*
-   * Regular Expressions: s.replaceAll("^,", ""); -- Remove leading comma
-   * s.matches(".+,.+") -- Match comma-seperated arguments in.split("[()]"); --
-   * in.split("[()]") -- Split with opening or closing bracket.
-   */
-
-  /*
-   * EXAMPLE INPUT STRINGS Read(false)
-   * Union(Sender("me@this.com",contains),Recipient("foo@bar.de",is))
-   * Intersection(Sender("mueller", startsWith), Read(true))
-   */
 
   public static boolean isOperator(String s) {
-    /*
-     * is: String ist identisch mit einem anderen String („NERD“ is „NERD“)
-     * contains: String enthält einen anderen String („NERD“ contains „ER“).
-     * contains not: String enthält nicht einen anderen String („NERD“ contains
-     * not „FOO“). starts with: String startet mit einem anderen String („NERD“
-     * starts with „NER“). ends with: String endet mit einem anderen String
-     * („NERD“ ends with „RD“).
-     */
-    return true;
+    for (FilterOperator operator : FilterOperator.values()) {
+      if (operator.value().toString().equals(s)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public static boolean isFilterName(String s) {
-    /*
-     * Sender: Gegeben ein String, filtere nach allen E-Mails, in denen die
-     * E-Mail Adresse (Message.sender.email) oder der Name des Absenders
-     * (Message.sender.personal) zu dem gegebenen String passt. Recipients:
-     * Gegeben ein String, filtere nach allen E-Mails, in denen die E-Mail
-     * Adresse (Message.recipients.email) oder Name der Empfänger
-     * (Message.recipients.personal) zu dem gegebenen String passt. Subject:
-     * Gegeben ein String, filtere nach allen E-Mails, deren Betreff
-     * (Message.subject) zu dem gegebenen String passt. Text: Gegeben ein
-     * String, filtere nach allen E-Mails, deren Text (Message.text) zu dem
-     * gegebenen String passt. Importance: Gegeben ein Literal der Enumeration
-     * de.bht.fpa.mail.s000000.common.mail.model.Importance, filtere nach alle
-     * E-Mails, die diese Wichtigkeit/Priorität haben (Message.importance).
-     * Read: Gegeben ein Boolean, filtere nach allen E-Mails, die gelesen sind
-     * (Message.isRead() == true) oder nicht (Message.isRead() == false).
-     * Intersection (Schnittmenge): Gegeben eine Liste von Filtern, wähle nur
-     * die E-Mails, die in der Ergebnismenge aller Filter enthalten sind. Dieser
-     * Filter kombiniert demnach andere Filter. Union (Vereinigung): G
-     */
-    return true;
-  }
-
-  public static boolean isSearchString(String s) {
-    // HELLO@WORLD.COM, etc.
-    return true;
+    for (FilterType type : FilterType.values()) {
+      if (type.value().equals(s)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   public static void main(String[] args) {
     String in = "Subject( Union( Importance(Subject(\"Huhu\")), Read(true) ), \"Mama\" )";
     LinkedList<ParsingList> pl = buildList(in);
     for (ParsingList pel : pl) {
+      System.out.println("--");
       for (String s : pel.getList()) {
         System.out.println(s);
       }
     }
   }
 
+  public static IFilter parse(LinkedList<ParsingList> results, IFilter filter) {
+
+    return filter;
+
+  }
+
   public static LinkedList<ParsingList> buildList(String input) {
     String[] expressions = input.split("[(),]");
-    // Visualize It!
-    for (String expr : expressions) {
-      String exprTr = expr.trim();
-      if (!exprTr.isEmpty()) {
-        System.out.println(exprTr);
-      }
-    }
     // Build List.
     LinkedList<ParsingList> results = new LinkedList<ParsingList>();
     ParsingList parsingList = new ParsingList();
