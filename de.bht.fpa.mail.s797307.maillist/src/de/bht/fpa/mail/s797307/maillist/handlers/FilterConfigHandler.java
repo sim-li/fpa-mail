14ac2c941936e48d955dedb8c69dddab692501a9
Simon Lischka <simon@a-studios.org>
2014-01-13 02:21:50 +0100

    Fix Keybindings

--- de.bht.fpa.mail.s797307.maillist/src/de/bht/fpa/mail/s797307/maillist/handlers/FilterConfigHandler.java
+++ de.bht.fpa.mail.s797307.maillist/src/de/bht/fpa/mail/s797307/maillist/handlers/FilterConfigHandler.java
@@ -5,30 +5,34 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
+
+import de.bht.fpa.mail.s000000.common.filter.FilterDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class FilterConfigHandler extends AbstractHandler {
-	/**
-	 * The constructor.
-	 */
-	public FilterConfigHandler() {
-	}
+  /**
+   * The constructor.
+   */
+  public FilterConfigHandler() {
+  }
 
-	/**
-	 * the command has been executed, so extract extract the needed information
-	 * from the application context.
-	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Maillist",
-				"Hello, Eclipse world");
-		return null;
-	}
+  /**
+   * the command has been executed, so extract extract the needed information
+   * from the application context.
+   */
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+    FilterDialog fd = new FilterDialog(window.getShell());
+    // MessageDialog.openInformation(
+    // window.getShell(),
+    // "Maillist",
+    // "Hello, Eclipse world");
+    return null;
+  }
 }
