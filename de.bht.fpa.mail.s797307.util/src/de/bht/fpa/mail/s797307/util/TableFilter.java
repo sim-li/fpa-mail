e6bde9b1cbb3618e5cebe17351d2b278dbe12c2d
Simon Lischka <simon@a-studios.org>
2014-01-14 00:34:21 +0100

    Improve Execution...

--- de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/TableFilter.java
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/TableFilter.java
@@ -1,36 +1,24 @@
 package de.bht.fpa.mail.s797307.util;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
-import de.bht.fpa.mail.s000000.common.filter.IFilter;
 import de.bht.fpa.mail.s000000.common.mail.model.Message;
 
 public class TableFilter extends ViewerFilter {
-  private IFilter filter;
+  private BasicFilter filter;
 
-  public TableFilter(IFilter filter) {
+  public TableFilter(BasicFilter filter) {
     this.filter = filter;
   }
 
-  public void setFilter(IFilter filter) {
+  public void setFilter(BasicFilter filter) {
     this.filter = filter;
   }
 
   @Override
   public boolean select(Viewer viewer, Object parentElement, Object element) {
-    Collection<Message> uselessCollection = new ArrayList<Message>();
-    if (element instanceof Message) {
-      uselessCollection.add((Message) element);
-      uselessCollection = filter.filter(uselessCollection);
-      if (uselessCollection.size() > 0) {
-        return true;
-      }
-    }
-    return false;
+    return filter.filter((Message) element);
   }
 
 }
