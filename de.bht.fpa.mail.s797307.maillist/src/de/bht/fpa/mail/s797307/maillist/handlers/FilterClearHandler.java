77dca60a3ec3d4c2004c7a6595a56084d14a4b8d
Simon Lischka <simon@a-studios.org>
2014-01-14 01:49:33 +0100

    New handler

---
+++ de.bht.fpa.mail.s797307.maillist/src/de/bht/fpa/mail/s797307/maillist/handlers/FilterClearHandler.java
@@ -0,0 +1,16 @@
+package de.bht.fpa.mail.s797307.maillist.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class FilterClearHandler extends AbstractHandler {
+
+  @Override
+  public Object execute(ExecutionEvent event) throws ExecutionException {
+    // TODO Auto-generated method stub
+    System.out.println("Huhu");
+    return null;
+  }
+
+}
