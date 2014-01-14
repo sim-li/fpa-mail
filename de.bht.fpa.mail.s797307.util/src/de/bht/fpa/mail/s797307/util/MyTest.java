16c46e04dd2caded3ab063ccebb1d75a2eb5e1d1
Simon Lischka <simon@a-studios.org>
2014-01-14 04:33:40 +0100

    Add JUnit Tests

--- de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MyTest.java
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MyTest.java
@@ -1,9 +1,9 @@
 package de.bht.fpa.mail.s797307.util;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
+import de.bht.fpa.mail.s000000.common.filter.FilterOperator;
 import de.bht.fpa.mail.s000000.common.mail.model.Message;
 
 public class MyTest {
@@ -23,4 +23,43 @@ public class MyTest {
     ReadFilter readFilter = new ReadFilter(true);
     assertTrue("ReadFilter filters for true", readFilter.filter(message));
   }
+
+  @Test
+  public void testSubjectRight() {
+    Message message = new Message();
+    message.setSubject("My pants are dancing");
+    SubjectFilter subjectFilter = new SubjectFilter("My", FilterOperator.STARTS_WITH);
+    assertTrue("ReadFilter filters correctly for right subject", subjectFilter.filter(message));
+  }
+
+  @Test
+  public void testSubjectWrong() {
+    Message message = new Message();
+    message.setSubject("My pants are dancing");
+    SubjectFilter subjectFilter = new SubjectFilter("aMyx", FilterOperator.STARTS_WITH);
+    assertFalse("ReadFilter filters correctly for wrong subject", subjectFilter.filter(message));
+  }
+
+  public void testInterSectionWrong() {
+    Message message = new Message();
+    message.setSubject("My pants are dancing");
+    message.setRead(true);
+    IntersectionFilter intersection = new IntersectionFilter();
+    SubjectFilter subjectFilter = new SubjectFilter("aMyx", FilterOperator.STARTS_WITH);
+    ReadFilter readFilter = new ReadFilter(true);
+    intersection.addFilters(subjectFilter, readFilter);
+    assertFalse("Intersection filter fails with wrong subject", intersection.filter(message));
+  }
+
+  public void testInterSectionRight() {
+    Message message = new Message();
+    message.setSubject("My pants are dancing");
+    message.setRead(true);
+    IntersectionFilter intersection = new IntersectionFilter();
+    SubjectFilter subjectFilter = new SubjectFilter("My", FilterOperator.STARTS_WITH);
+    ReadFilter readFilter = new ReadFilter(true);
+    intersection.addFilters(subjectFilter, readFilter);
+    assertTrue("Intersection filter succeeds with right subject", intersection.filter(message));
+  }
+
 }
