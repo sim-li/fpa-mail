6ef37ca286058734593c423be88db5da7ac6e2b2
Simon Lischka <simon@a-studios.org>
2014-01-25 07:45:00 +0100

    First commit

---
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MFolder.java
@@ -0,0 +1,34 @@
+package de.bht.fpa.mail.s797307.util;
+
+import java.util.List;
+
+import de.bht.fpa.mail.s000000.common.mail.model.Folder;
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+
+public class MFolder implements MTargetNode {
+
+	@Override
+	public List<Object> getChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getMessages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasFolder() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Folder getFolder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
