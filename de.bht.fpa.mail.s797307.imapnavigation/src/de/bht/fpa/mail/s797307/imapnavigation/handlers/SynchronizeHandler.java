2f9137192501345f751f355ef3ff6955257d1d24
Simon Lischka <simon@a-studios.org>
2014-01-25 21:55:39 +0100

    Add new files

---
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/handlers/SynchronizeHandler.java
@@ -0,0 +1,16 @@
+package de.bht.fpa.mail.s797307.imapnavigation.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class SynchronizeHandler extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Bitches");
+		return null;
+	}
+
+}
