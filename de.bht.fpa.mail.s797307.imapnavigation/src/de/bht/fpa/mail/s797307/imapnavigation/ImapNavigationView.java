186faa93ae1e63462f36726a1fe9c0c3e59a2112
Simon Lischka <simon@a-studios.org>
2014-01-31 00:29:22 +0100

    Id business

--- de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/ImapNavigationView.java
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/ImapNavigationView.java
@@ -1,18 +1,14 @@
 package de.bht.fpa.mail.s797307.imapnavigation;
 
-import java.util.LinkedList;
-import java.util.List;
-
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.part.ViewPart;
 
-import de.bht.fpa.mail.s000000.common.mail.model.Account;
-import de.bht.fpa.mail.s000000.common.mail.model.Folder;
-import de.bht.fpa.mail.s000000.common.mail.model.builder.AccountBuilder;
-import de.bht.fpa.mail.s000000.common.mail.model.builder.FolderBuilder;
 import de.bht.fpa.mail.s000000.common.mail.testdata.RandomTestDataProvider;
 import de.bht.fpa.mail.s797307.util.MAccount;
 import de.bht.fpa.mail.s797307.util.MTargetNode;
@@ -27,44 +23,58 @@ public final class ImapNavigationView extends ViewPart {
     viewer = new TreeViewer(parent);
     viewer.setLabelProvider(l);
     viewer.setContentProvider(cp);
-    viewer.setInput(createModel());
+//    viewer.setInput(createModel());
+    initializeSync();
     getSite().setSelectionProvider(viewer);
     initalizeExecutionListener();
   }
 
-  private MTargetNode createModel() {
-	 
+//  private MTargetNode createModel() {
+  private void initializeSync() {
 	  RandomTestDataProvider data = new RandomTestDataProvider(20);
-	  
-	 
-	  
-
-	  
-	  
-	  
-	  Folder sent = FolderBuilder.newFolderBuilder()
-			  .id(4711L)
-			     .fullName("Sent").builtMessages(data.getMessages()).build();
-	  
-	  Folder in = FolderBuilder.newFolderBuilder()
-			  .id(4718L)
-			     .fullName("In").build();
-	  in.setMessages(data.getMessages());
-	  
-	  List <Folder> folders = new LinkedList <Folder>();
-	  folders.add(sent);
-	  folders.add(in);
-	  
-	  Account account = AccountBuilder.newAccountBuilder()
-		      .id(4711L)
-		      	.name("Alice-IMAP")
-		      		.host("googlemail.com").build();
-	  account.setFolders(folders);
-	  
-	  
-	  MTargetNode rootNode = new MAccount(account);
-	  return rootNode;
+	  Job.getJobManager().addJobChangeListener(new JobChangeAdapter() {
+		  public void done(IJobChangeEvent event) {
+			  if (event.getJob().getName() == "MailJob") {
+				  if (event.getResult().isOK()) {
+					  MailJob job = (MailJob) event.getJob();
+					  System.out.println(job.getAccount().getId());
+//					  MTargetNode rootNode = new MAccount(job.getAccount());
+//					  System.out.println(rootNode.getMessages().isEmpty());
+//					  viewer.setInput(rootNode);
+//					  viewer.refresh();
+					  System.out.println("Job completed successfully, viewer fresh.");
+				  } else {
+					  System.out.println("Job did not complete successfully");
+				  }
+			  }
+		  }
+	  });	
   }
+	  
+////	  
+////	  Folder sent = FolderBuilder.newFolderBuilder()
+////			  .id(4711L)
+////			     .fullName("Sent").builtMessages(data.getMessages()).build();
+////	  
+////	  Folder in = FolderBuilder.newFolderBuilder()
+////			  .id(4718L)
+////			     .fullName("In").build();
+////	  in.setMessages(data.getMessages());
+////	  
+////	  List <Folder> folders = new LinkedList <Folder>();
+////	  folders.add(sent);
+////	  folders.add(in);
+////	  
+////	  Account account = AccountBuilder.newAccountBuilder()
+////		      .id(4711L)
+////		      	.name("Alice-IMAP")
+////		      		.host("googlemail.com").build();
+////	  account.setFolders(folders);
+////	  
+//	  
+////	  MTargetNode rootNode = new MAccount(account);
+//	  return rootNode;
+//  }
 
   public void setInput(Object input) {
     viewer.setInput(input);
--- de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/handlers/SynchronizeHandler.java
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/handlers/SynchronizeHandler.java
@@ -8,11 +8,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 
 import de.bht.fpa.mail.s000000.common.mail.imapsync.ImapHelper;
 import de.bht.fpa.mail.s000000.common.mail.imapsync.SynchronizationException;
@@ -22,6 +18,7 @@ import de.bht.fpa.mail.s000000.common.mail.model.Message;
 import de.bht.fpa.mail.s000000.common.mail.model.builder.AccountBuilder;
 import de.bht.fpa.mail.s000000.common.mail.model.builder.FolderBuilder;
 import de.bht.fpa.mail.s000000.common.mail.testdata.RandomTestDataProvider;
+import de.bht.fpa.mail.s797307.imapnavigation.MailJob;
 
 public class SynchronizeHandler extends AbstractHandler{
 
@@ -43,40 +40,13 @@ public class SynchronizeHandler extends AbstractHandler{
 	
 	  final Account account = AccountBuilder.newAccountBuilder()
 		      .id(4711L)
-		      	.name("Google Mail")
-		      		.host("imap.googlemail.com")
-		      		  .username("bhtfpa@googlemail.com")
-		      		  	.password("B-BgxkT_anr2bubbyTLM")
+		      	.name("FPA Demo")
+		      		.host("imap.a-studios.org")
+		      		  .username("fpademo@a-studios.org")
+		      		  	.password("fpademo")
 		      		  		.build();
-	  Job job = new Job("Long running Job") {
-		  @Override
-		  protected IStatus run(IProgressMonitor monitor) {
-			  try {
-				  monitor.beginTask("Doing fancy stuff, you fucker", 100);
-				  ImapHelper.setDebug(true);
-				  ImapHelper.syncAllFoldersToAccount(account, monitor);
-				  if (monitor.isCanceled()) {
-					  return Status.CANCEL_STATUS;
-				  }
-				  return Status.OK_STATUS;
-			  }
-			  catch (SynchronizationException e) {
-				  System.err.println("Legend of the phoenix.");
-				  return Status.CANCEL_STATUS;
-			  } finally {
-				  monitor.done();
-			  }
-		  }
-	  };
-
-	  job.addJobChangeListener(new JobChangeAdapter() {
-	        public void done(IJobChangeEvent event) {
-	        if (event.getResult().isOK())
-	           System.out.println("Job completed successfully");
-	           else
-	              System.out.println("Job did not complete successfully");
-	        }
-	     });
+	  
+	  Job job = new MailJob(account);
 	  job.schedule();
 	return null;
 	  
