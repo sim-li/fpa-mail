f6f2bc652f9f13579d1b4ca4f0b3f2c47af4ee4c
Simon Lischka <simon@a-studios.org>
2014-01-31 21:58:00 +0100

    Add schmanzy Dialog

---
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/NewMessageView.java
@@ -0,0 +1,66 @@
+package de.bht.fpa.mail.s797307.imapnavigation;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class NewMessageView extends Dialog { 
+	 private Composite container;
+	 private Text text;
+	 private Text text_1;
+	 private static final int HEIGHT = 500;
+	 private static final int WIDTH = 600;
+	 private Text text_2;
+	 private Label lblMessage;
+	public NewMessageView(Shell parentShell) {
+		super(parentShell);
+		setShellStyle(SWT.MAX | SWT.RESIZE | SWT.TITLE | SWT.APPLICATION_MODAL);
+	}
+	
+	 @Override
+	  protected Control createDialogArea(Composite parent) {
+	    container = (Composite) super.createDialogArea(parent);
+	    container.setLayout(new GridLayout(2, false));
+	    new Label(container, SWT.NONE);
+	    
+	    Label lblTo = new Label(container, SWT.NONE);
+	    lblTo.setText("Recipient:");
+	    new Label(container, SWT.NONE);
+	    
+	    text = new Text(container, SWT.BORDER);
+	    text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+	    new Label(container, SWT.NONE);
+	    
+	    Label lblSubject = new Label(container, SWT.NONE);
+	    lblSubject.setText("Subject:");
+	    new Label(container, SWT.NONE);
+	    
+	    text_1 = new Text(container, SWT.BORDER);
+	    text_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+	    new Label(container, SWT.NONE);
+	    
+	    lblMessage = new Label(container, SWT.NONE);
+	    lblMessage.setText("Message:");
+	    new Label(container, SWT.NONE);
+	    
+	    text_2 = new Text(container, SWT.BORDER | SWT.WRAP | SWT.MULTI);
+	    GridData gd_text_2 = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+	    gd_text_2.heightHint = 292;
+	    text_2.setLayoutData(gd_text_2);
+	    getShell().setText("Write new e-mail");
+	    return container;
+	  }
+
+	protected Point getInitialSize() {
+		return new Point(715, 489);
+	}
+}
