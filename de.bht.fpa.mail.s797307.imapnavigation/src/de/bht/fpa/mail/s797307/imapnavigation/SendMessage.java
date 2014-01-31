7ec4d7e9219a0c218e85be79e176e888a4890f10
Simon Lischka <simon@a-studios.org>
2014-02-01 00:30:26 +0100

    Some debugging necessary

--- de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/SendMessage.java
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/SendMessage.java
@@ -2,7 +2,6 @@ package de.bht.fpa.mail.s797307.imapnavigation;
 
 import java.util.Properties;
 
-import javax.mail.AuthenticationFailedException;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -10,46 +9,40 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import de.bht.fpa.mail.s000000.common.mail.model.Account;
+
 public final class SendMessage {
  
-	public static void send(String recipient, String subject, String text) {
-//		String host = "smtp.googlemail.com";
-//		String username = "bhtfpa@googlemail.com";
-//		String password = "B-BgxkT_anr2bubbyTLM";
-		String host = "smtp.mail.yahoo.com";
-		String username = "funkjaymatada@ymail.com";
-		String password = "sonysaqz";
+	public static void send(Account account, String recipient, String subject, String text) {
 		Properties props = new Properties();
-//		props.put("mail.smtp.host", "smtp.googlemail.com");
+		System.out.println(account.getHost());
+		System.out.println(account.getUsername());
+		System.out.println(account.getPassword());
+		props.put("mail.smtp.host", account.getHost());
 		props.put("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.socketFactory.class",
 				"javax.net.ssl.SSLSocketFactory");
-//		props.put("mail.smtp.auth", "true");
-//		props.put("mail.ssl.auth", "true");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.ssl.auth", "true");
 		props.put("mail.smtp.port", "465");
- 
 		Session session = Session.getDefaultInstance(props, null);
- 
 		try {
-			
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("funkjaymatada@ymail.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("simon@a-studios.org"));
-			message.setSubject("Hola123");
-			message.setText("Testing the west.");
-			
+			Message msg = new MimeMessage(session);
+			msg.setRecipients(Message.RecipientType.TO,
+					InternetAddress.parse(recipient));
+			msg.setSubject(subject);
+			msg.setText(text);
+			System.out.println(recipient + ">" + subject  + ">" + text);
 			Transport transport = session.getTransport("smtp");
-            transport.connect(host, username, password);
-            transport.sendMessage(message, message.getAllRecipients());
+            transport.connect(account.getHost(), account.getUsername(), account.getPassword());
+            transport.sendMessage(msg, msg.getAllRecipients());
             transport.close();
-			Transport.send(message);
- 
+			Transport.send(msg);
 			System.out.println("Done");
  
 		} catch (MessagingException e) {
-//			throw new RuntimeException(e);
-			System.out.println("Hey you know I got some prob here.");
+			throw new RuntimeException(e);
+//			System.out.println("Hey you know I got some prob here.");
 		}
 		
 	}
--- de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/handlers/SendmailHandler.java
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/handlers/SendmailHandler.java
@@ -1,5 +1,8 @@
 package de.bht.fpa.mail.s797307.imapnavigation.handlers;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -7,6 +10,9 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
+import de.bht.fpa.mail.s000000.common.mail.model.Account;
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+import de.bht.fpa.mail.s000000.common.mail.model.Recipient;
 import de.bht.fpa.mail.s797307.imapnavigation.NewMessageView;
 import de.bht.fpa.mail.s797307.imapnavigation.SendMessage;
 
@@ -17,7 +23,19 @@ public class SendmailHandler extends AbstractHandler {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 	    NewMessageView dialog = new  NewMessageView(window.getShell());
 	    if (dialog.open() == Window.OK) {
-	    	SendMessage.send(dialog.getRecipient(), dialog.getSubject(), dialog.getMessage());
+	    	Account gmailSend = new Account();
+	    	gmailSend.setHost("smtp.googlemail.com");
+	    	gmailSend.setUsername("bhtfpa@googlemail.com");
+	    	gmailSend.setPassword("B-BgxkT_anr2bubbyTLM");	
+	    	gmailSend.setName("bhtfpa@googlemail.com");
+	    	
+	    	Account ySend = new Account();
+	    	ySend.setHost("smtp.mail.yahoo.com");
+		    ySend.setUsername("funkjaymatada@ymail.com");
+		    gmailSend.setName("funkjaymatada@ymail.com");
+		    ySend.setPassword("sonysaqz");
+		    SendMessage.send(ySend, "simon@a-studios.org", "Testing the west", "Doing it again");
+//	    	SendMessage.send(ySend, dialog.getRecipient(), dialog.getSubject(), dialog.getMessage());
 	    }
 	    return null;
 	}
