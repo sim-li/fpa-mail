eeba027cbf49fcb2b2cd2c87cb75067715418e07
Simon Lischka <simon@a-studios.org>
2014-01-31 10:48:46 +0100

    Sync mails working state

--- de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/ImapNavigationView.java
+++ de.bht.fpa.mail.s797307.imapnavigation/src/de/bht/fpa/mail/s797307/imapnavigation/ImapNavigationView.java
@@ -5,91 +5,65 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.ICommandService;
 import org.eclipse.ui.part.ViewPart;
 
-import de.bht.fpa.mail.s000000.common.mail.testdata.RandomTestDataProvider;
+import de.bht.fpa.mail.s000000.common.mail.imapsync.ImapHelper;
+import de.bht.fpa.mail.s000000.common.mail.model.Account;
 import de.bht.fpa.mail.s797307.util.MAccount;
-import de.bht.fpa.mail.s797307.util.MTargetNode;
 
 public final class ImapNavigationView extends ViewPart {
-  private TreeViewer viewer;
+	private TreeViewer viewer;
 
-  @Override
-  public void createPartControl(Composite parent) {
-    ImapViewLabelProvider l = new ImapViewLabelProvider();
-    ImapContentProvider cp = new ImapContentProvider(this);
-    viewer = new TreeViewer(parent);
-    viewer.setLabelProvider(l);
-    viewer.setContentProvider(cp);
-//    viewer.setInput(createModel());
-    initializeSync();
-    getSite().setSelectionProvider(viewer);
-    initalizeExecutionListener();
-  }
+	@Override
+	public void createPartControl(Composite parent) {
+		ImapViewLabelProvider l = new ImapViewLabelProvider();
+		ImapContentProvider cp = new ImapContentProvider(this);
+		viewer = new TreeViewer(parent);
+		viewer.setLabelProvider(l);
+		viewer.setContentProvider(cp);
+		initializeSync();
+		getSite().setSelectionProvider(viewer);
+		initalizeExecutionListener();
+	}
 
-//  private MTargetNode createModel() {
-  private void initializeSync() {
-	  RandomTestDataProvider data = new RandomTestDataProvider(20);
-	  Job.getJobManager().addJobChangeListener(new JobChangeAdapter() {
-		  public void done(IJobChangeEvent event) {
-			  if (event.getJob().getName() == "MailJob") {
-				  if (event.getResult().isOK()) {
-					  MailJob job = (MailJob) event.getJob();
-					  System.out.println(job.getAccount().getId());
-					  System.out.println(job.getAccount().getFolders().get(0).getMessages().get(0).getSubject());
-//					  MTargetNode rootNode = new MAccount(job.getAccount());
-//					  System.out.println(rootNode.getMessages().isEmpty());
-//					  viewer.setInput(rootNode);
-//					  viewer.refresh();
-					  System.out.println("Job completed successfully, viewer fresh.");
-				  } else {
-					  System.out.println("Job did not complete successfully");
-				  }
-			  }
-		  }
-	  });	
-  }
-	  
-////	  
-////	  Folder sent = FolderBuilder.newFolderBuilder()
-////			  .id(4711L)
-////			     .fullName("Sent").builtMessages(data.getMessages()).build();
-////	  
-////	  Folder in = FolderBuilder.newFolderBuilder()
-////			  .id(4718L)
-////			     .fullName("In").build();
-////	  in.setMessages(data.getMessages());
-////	  
-////	  List <Folder> folders = new LinkedList <Folder>();
-////	  folders.add(sent);
-////	  folders.add(in);
-////	  
-////	  Account account = AccountBuilder.newAccountBuilder()
-////		      .id(4711L)
-////		      	.name("Alice-IMAP")
-////		      		.host("googlemail.com").build();
-////	  account.setFolders(folders);
-////	  
-//	  
-////	  MTargetNode rootNode = new MAccount(account);
-//	  return rootNode;
-//  }
+	private void initializeSync() {
+		Job.getJobManager().addJobChangeListener(new JobChangeAdapter() {
+			public void done(IJobChangeEvent event) {
+				final IJobChangeEvent myEvent = event;
+				if (event.getJob().getName() == "MailJob") {
+					Display.getDefault().asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							if (myEvent.getResult().isOK()) {
+								Account account = ImapHelper
+										.getAccount("FPA Demo");
+								MAccount accountNode = new MAccount(account);
+								viewer.setInput(accountNode);
+							} else {
+								System.err
+										.println("Hey man, I've got a problem retrieving your mails.");
+							}
+						}
+					});
 
-  public void setInput(Object input) {
-    viewer.setInput(input);
-  }
+				}
+			}
+		});
+	}
 
-  public void initalizeExecutionListener() {
-    ICommandService commandService = (ICommandService) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-        .getService(ICommandService.class);
-    commandService.addExecutionListener(new ExecutionController(viewer));
-  }
+	public void initalizeExecutionListener() {
+		ICommandService commandService = (ICommandService) PlatformUI
+				.getWorkbench().getActiveWorkbenchWindow()
+				.getService(ICommandService.class);
+		commandService.addExecutionListener(new ExecutionController(viewer));
+	}
 
-  @Override
-  public void setFocus() {
-    // TODO Auto-generated method stub
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
 
-  }
+	}
 }
