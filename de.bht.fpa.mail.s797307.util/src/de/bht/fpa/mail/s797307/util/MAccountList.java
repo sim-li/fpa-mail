52f6500bd44fcbcb918f5d1f6c90325f13967a1f
Simon Lischka <simon@a-studios.org>
2014-01-31 19:15:55 +0100

    Other list

--- de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MAccountList.java
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MAccountList.java
@@ -3,40 +3,22 @@ package de.bht.fpa.mail.s797307.util;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import de.bht.fpa.mail.s000000.common.mail.model.Account;
 import de.bht.fpa.mail.s000000.common.mail.model.Message;
 
-@XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement(name = "accountsWu")
 public class MAccountList implements MTargetNode {
-	private static final long serialVersionUID = -2160650637811169762L;
-	
 	private List <MAccount> elements;
-	
-    @XmlElement(name = "account", type = Account.class)
-	private List <Account> baseElements;
-	
 
 	public MAccountList() {
 		elements = new LinkedList <MAccount>();
-		baseElements = new LinkedList <Account>();
 	}
 	
 	public void swipeList() {
 		elements = new LinkedList <MAccount>();
-		for (Account account : baseElements) {
-			elements.add(new MAccount(account));
-		}
 	}
 	
 	@Override
 	public List<?> getChildren() {
-		System.out.println("Got children: " + elements.size());
 		return elements;
 	}
 
@@ -51,17 +33,14 @@ public class MAccountList implements MTargetNode {
 	
 	public void add(MAccount element) {
 		elements.add(element);
-		baseElements.add(Tools.clearOutAccount((Account) element.getElement()));
 	}
 	
 	public void add(Account element) {
 		elements.add(new MAccount(element));
-		baseElements.add(element);
 	}
 	
 	public void remove(MTargetNode element) {
 		elements.remove(element);
-		baseElements.remove((Account) element.getElement());
 	}
 	
 	@Override
---
+++ de.bht.fpa.mail.s797307.util/src/de/bht/fpa/mail/s797307/util/MBaseAccountList.java
@@ -0,0 +1,39 @@
+package de.bht.fpa.mail.s797307.util;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import de.bht.fpa.mail.s000000.common.mail.model.Account;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "simonsaccountshop")
+public class MBaseAccountList {
+	private static final long serialVersionUID = -2160650637811169762L;
+	
+    @XmlElement(name = "account", type = Account.class)
+	private List <Account> baseElements;
+	
+
+	public MBaseAccountList(MAccountList accounts) {
+		baseElements = new LinkedList <Account>();
+		for (Object account : accounts.getElements()) {
+			MAccount element = (MAccount) account;
+			baseElements.add((Account) element.getElement()); 
+		}
+	}
+	
+	public MAccountList getMAccountList() {
+	    MAccountList retrievedAccounts = new MAccountList();
+		for (Account account : baseElements) {
+			retrievedAccounts.add(new MAccount(account));
+		}
+		return retrievedAccounts;
+	}
+	
+
+}
